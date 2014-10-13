f2001d7057b31a33a93266893477b17ec5474b03
Jason Trost <jason.trost@gmail.com>
2014-10-13 13:30:36 -0400

    added a precompute script

--- scripts/precompute_counts_migration.js
+++ scripts/precompute_counts_migration.js
@@ -1,63 +1,63 @@
 /*
-	This script pre-computes the counts from the hpfeeds data over time.
+    This script pre-computes the counts from the hpfeeds data over time.
 */
 
 var pad = function(number){
-	if(number < 10) {
-		return "0"+String(number);
-	}
-	else {
-		return String(number);
-	}
+    if(number < 10) {
+        return "0"+String(number);
+    }
+    else {
+        return String(number);
+    }
 };
 
 var transform = function(rec) {
-	var identifier = rec['_id']['identifier'];
+    var identifier = rec['_id']['identifier'];
 
-	var year    = pad(rec['_id']['year']);
-	var month   = pad(rec['_id']['month']);
-	var day     = pad(rec['_id']['day']);
+    var year    = pad(rec['_id']['year']);
+    var month   = pad(rec['_id']['month']);
+    var day     = pad(rec['_id']['day']);
 
-	var date = year+month+day;
+    var date = year+month+day;
 
-	db.counts.update({'date': date, 'identifier': identifier}, {'$set': {'event_count': rec['event_count']}}, {'upsert': true, 'multi': false});
+    db.counts.update({'date': date, 'identifier': identifier}, {'$set': {'event_count': rec['event_count']}}, {'upsert': true, 'multi': false});
 };
 
 
 var groupby_channel = {
-	'$group': {
-		'_id':{
-			'identifier': "$channel",
-			'year': {
-				'$year': "$timestamp"
-			},
-			'month':{
-				'$month':"$timestamp"
-			},
-			'day':{
-				'$dayOfMonth':"$timestamp"
-			}
-		},
-		event_count: {$sum: 1}
-	}
+    '$group': {
+        '_id':{
+            'identifier': "$channel",
+            'year': {
+                '$year': "$timestamp"
+            },
+            'month':{
+                '$month':"$timestamp"
+            },
+            'day':{
+                '$dayOfMonth':"$timestamp"
+            }
+        },
+        event_count: {$sum: 1}
+    }
 };
 
 var groupby_ident = {
-	'$group': {
-		'_id':{
-			'identifier': "$ident",
-			'year': {
-				'$year': "$timestamp"
-			},
-			'month':{
-				'$month':"$timestamp"
-			},
-			'day':{
-				'$dayOfMonth':"$timestamp"
-			}
-		},
-		event_count: {$sum: 1}
-	}
+    '$group': {
+        '_id':{
+            'identifier': "$ident",
+            'year': {
+                '$year': "$timestamp"
+            },
+            'month':{
+                '$month':"$timestamp"
+            },
+            'day':{
+                '$dayOfMonth':"$timestamp"
+            }
+        },
+        event_count: {$sum: 1}
+    }
 };
 
 
