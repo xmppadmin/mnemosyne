d85d06710627672dad8329667b655287513ae396
Jason Trost <jason.trost@gmail.com>
2015-06-19 08:36:41 -0400

    check for list as well in basenormalizer.py.parse_record_data

--- normalizer/modules/basenormalizer.py
+++ normalizer/modules/basenormalizer.py
@@ -79,5 +79,7 @@ class BaseNormalizer(object):
     def parse_record_data(self, data):
         if type(data) is dict:
             return data
+        elif type(data) is list:
+            return data
         else:
             return json.loads(data)
